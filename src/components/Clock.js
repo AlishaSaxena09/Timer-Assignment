aae6cf4f9edcae7ce6b4fe381f615efcf1fea120
Alisha <alishasaxena2016@gmail.com>
2023-11-03 00:54:53 +0530

    ref: add box shadow

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -35,59 +35,61 @@ const chartData = calculateDataForChart(timeSlots);
 export default function Clock() {
   return (
     // Responsive container doesn't support rendering list
-    // <ResponsiveContainer width="100%" height="100%">
-    <div className="clock-container">
-      {chartData.map((record) => {
-        return (
-          <RadialBarChart
-            height={200}
-            width={200}
-            cx="50%"
-            cy="50%"
-            innerRadius={80}
-            outerRadius={200}
-            barSize={10}
-            data={[record]}
-            startAngle={record.startAngle}
-            endAngle={record.endAngle}
-          >
-            <RadialBar
-              cornerRadius={"50%"}
-              background
-              clockWise
-              dataKey="value"
-            />
-          </RadialBarChart>
-        );
-      })}
-      <div className="divider first" />
-      <div className="divider second" />
-      <div className="clock-border" />
+    // <ResponsiveContainer width="100%" height="100%">\
+    <div className="container">
+      <div className="clock-container">
+        {chartData.map((record) => {
+          return (
+            <RadialBarChart
+              height={200}
+              width={200}
+              cx="50%"
+              cy="50%"
+              innerRadius={80}
+              outerRadius={200}
+              barSize={10}
+              data={[record]}
+              startAngle={record.startAngle}
+              endAngle={record.endAngle}
+            >
+              <RadialBar
+                cornerRadius={"50%"}
+                background
+                clockWise
+                dataKey="value"
+              />
+            </RadialBarChart>
+          );
+        })}
+        <div className="divider first" />
+        <div className="divider second" />
+        <div className="clock-border" />
 
-      <div className="test1">Night</div>
-      <div className="test3">Morning</div>
-      <div className="test2">Afternoon</div>
-      <div className="test4">Evening</div>
-      {new Array(48).fill(null).map((_, i) => {
-        if (i % 4 === 0) {
+        <div className="test1">Night</div>
+        <div className="test3">Morning</div>
+        <div className="test2">Afternoon</div>
+        <div className="test4">Evening</div>
+        {new Array(48).fill(null).map((_, i) => {
+          if (i % 4 === 0) {
+            return (
+              <div
+                className={`labelled-hour-marker ${
+                  i % 12 === 0 ? "bold-label" : ""
+                }`}
+                style={{ "--deg": `${i * 7.5}deg` }}
+              >
+                <span>{i / 2}</span>
+              </div>
+            );
+          }
           return (
             <div
-              className={`labelled-hour-marker ${
-                i % 12 === 0 ? "bold-label" : ""
-              }`}
+              className="half-hour-marker"
               style={{ "--deg": `${i * 7.5}deg` }}
-            >
-              <span>{i / 2}</span>
-            </div>
+            />
           );
-        }
-        return (
-          <div
-            className="half-hour-marker"
-            style={{ "--deg": `${i * 7.5}deg` }}
-          />
-        );
-      })}
+        })}
+      </div>
     </div>
   );
 }
